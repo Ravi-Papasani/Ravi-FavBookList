c1e57593f4030d5471d7d6efa6abcc126f119f1d
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-01-23 16:24:05 -0600

    UI class to display books list when DOMcontentLoaded

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //Book Class: used to handle books
 
 class Book {
-
-    constructor(ipt_tile,ipt_author,ipt_isbn){
+    constructor(ipt_tile, ipt_author, ipt_isbn) {
         this.title = ipt_tile;
         this.author = ipt_author;
         this.isbn = ipt_isbn
@@ -12,9 +11,55 @@ class Book {
 
 //UI Class: used to handle UI tasks
 
+class UI {
+    //displayBooks Method
+    static displayBooks() {
+      const storedBooks = [
+          {title: 'The Alchemist',
+           author: 'Paulo Coelho',
+           isbn: '11111' },
+           {title: 'The Leader In You',
+           author: 'Dale Carnegie',
+           isbn: '22222'}
+      ];
+      //books variable to store the storedBooks
+      const books = storedBooks;
+      
+      //looping the books stored using foreach loop using array method and calling addBookToList method
+      books.forEach((book) => UI.addBookToList(book));
+    }
+    
+    //addBookToList Method
+    static addBookToList(book) {
+        
+        //grabbing the DOM element #book-list and storing to list variable
+        const list = document.querySelector('#book-list');
+
+        //Creating a table row element
+        const row = document.createElement('tr');
+        
+        //adding Html text using backsticks `` so that we can use variables inside the string
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td>
+        <a href='#'>X</a>
+        </td>
+        `;
+
+        //Append the row to the list
+        list.appendChild(row);
+    }
+
+}
+
 //Storage class: used to store/retrive books
 
 //Event: Display Books
+
+//addEventListener for DOMContentLoaded then call Ui displaybook method
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a new Book
 
