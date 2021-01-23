db9ced018ddb058cb5959741bfaf9eb3b4735ff3
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-01-23 17:04:58 -0600

    Add a new book to the list when form submit event happened

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //Book Class: used to handle books
-
 class Book {
     constructor(ipt_tile, ipt_author, ipt_isbn) {
         this.title = ipt_tile;
@@ -10,34 +9,36 @@ class Book {
 
 
 //UI Class: used to handle UI tasks
-
 class UI {
     //displayBooks Method
     static displayBooks() {
-      const storedBooks = [
-          {title: 'The Alchemist',
-           author: 'Paulo Coelho',
-           isbn: '11111' },
-           {title: 'The Leader In You',
-           author: 'Dale Carnegie',
-           isbn: '22222'}
-      ];
-      //books variable to store the storedBooks
-      const books = storedBooks;
-      
-      //looping the books stored using foreach loop using array method and calling addBookToList method
-      books.forEach((book) => UI.addBookToList(book));
+        const storedBooks = [{
+                title: 'The Alchemist',
+                author: 'Paulo Coelho',
+                isbn: '11111'
+            },
+            {
+                title: 'The Leader In You',
+                author: 'Dale Carnegie',
+                isbn: '22222'
+            }
+        ];
+        //books variable to store the storedBooks
+        const books = storedBooks;
+
+        //looping the books stored using foreach loop using array method and calling addBookToList method
+        books.forEach((book) => UI.addBookToList(book));
     }
-    
+
     //addBookToList Method
     static addBookToList(book) {
-        
+
         //grabbing the DOM element #book-list and storing to list variable
         const list = document.querySelector('#book-list');
 
         //Creating a table row element
         const row = document.createElement('tr');
-        
+
         //adding Html text using backsticks `` so that we can use variables inside the string
         row.innerHTML = `
         <td>${book.title}</td>
@@ -54,13 +55,35 @@ class UI {
 
 }
 
+
 //Storage class: used to store/retrive books
 
-//Event: Display Books
 
+
+//Event: Display Books
 //addEventListener for DOMContentLoaded then call Ui displaybook method
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
+
 //Event: Add a new Book
+//DOM ID book-form addEventListener submit happened then trigger the arrow event function 
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //prevent the actual sumbit event
+    e.preventDefault();
+
+    // Getting the form values for title,author,isbn
+    // storing the DOM title value(input.value()) entered in the book form 
+    const ipt_title = document.querySelector('#title').value;
+    const ipt_author = document.querySelector('#author').value;
+    const ipt_isbn = document.querySelector('#isbn').value;
+
+    //Now instatiate above form value to Book constructor
+    const book = new Book(ipt_title, ipt_author, ipt_isbn);
+
+    //add the above book Object to UI 
+    UI.addBookToList(book);
+
+});
+
 
 //Event: Remove a Book from the list
