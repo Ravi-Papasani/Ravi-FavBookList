4169d2dfee488be725d7c7c7a82fada49c4e7f18
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-01-23 15:30:34 -0600

    Book class constructor to Instantiate book values

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 //Book Class: used to handle books
 
+class Book {
+
+    constructor(ipt_tile,ipt_author,ipt_isbn){
+        this.title = ipt_tile;
+        this.author = ipt_author;
+        this.isbn = ipt_isbn
+    }
+}
+
 
 //UI Class: used to handle UI tasks
 
