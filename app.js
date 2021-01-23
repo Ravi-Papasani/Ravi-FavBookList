4bf12960bf40d5b1fec7ec34aaa353f974fdfc0f
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2021-01-23 15:20:31 -0600

    Html view and js tasks

---
+++ app.js
@@ -0,0 +1,12 @@
+//Book Class: used to handle books
+
+
+//UI Class: used to handle UI tasks
+
+//Storage class: used to store/retrive books
+
+//Event: Display Books
+
+//Event: Add a new Book
+
+//Event: Remove a Book from the list
